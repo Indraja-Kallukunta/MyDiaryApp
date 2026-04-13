eb61f40f59e286351e54a3d1794cff60002887f3
Indraja-Kallukunta <kindraja399@gmail.com>
2026-04-13 08:45:37 +0530

    EntriesServiceImpl.java

--- src/main/java/com/twg/Springboot/Service/EntriesServiceImpl.java
+++ src/main/java/com/twg/Springboot/Service/EntriesServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.twg.Springboot.Entities.EntriesOfUser;
-import com.twg.Springboot.Entities.User3;
+import com.twg.Springboot.Entities.Users;
 import com.twg.Springboot.Repository.EntriesInterface;
 
 @Service
