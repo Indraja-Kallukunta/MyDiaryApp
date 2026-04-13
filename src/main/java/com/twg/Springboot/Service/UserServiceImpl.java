a56788c2a0c8a1fba3fe2675a6f49ad582028780
Indraja-Kallukunta <kindraja399@gmail.com>
2026-04-13 08:48:02 +0530

    UserServiceImpl.java

--- src/main/java/com/twg/Springboot/Service/UserServiceImpl.java
+++ src/main/java/com/twg/Springboot/Service/UserServiceImpl.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.twg.Springboot.Entities.User3;
+import com.twg.Springboot.Entities.Users;
 import com.twg.Springboot.Repository.UserInterface;
 
 @Service
@@ -14,38 +14,38 @@ public class UserServiceImpl implements UserServiceInterface {
 	@Autowired
 	private UserInterface userInterface;
 	@Override
-	public User3 saveUser(User3 user) {
-		 User3 user1 = userInterface.save(user);
+	public Users saveUser(Users user) {
+		 Users user1 = userInterface.save(user);
 		return user1;
 	}
 
 	@Override
-	public User3 updateUser(User3 user) {
-		User3 user1 = userInterface.save(user);
+	public Users updateUser(Users user) {
+		Users user1 = userInterface.save(user);
 		return user1;
 	}
 
 	@Override
-	public void deleteUser(User3 user) {
+	public void deleteUser(Users user) {
 		userInterface.delete(user);
 	}
 
 	@Override
-	public User3 getUserById(long id) {
-		User3 user1 = userInterface.findById(id).get();
+	public Users getUserById(long id) {
+		Users user1 = userInterface.findById(id).get();
 		return user1;
 	}
 
 
 	@Override
-	public List<User3> findAllUsers() {
-		List<User3> user1 = userInterface.findAll();
+	public List<Users> findAllUsers() {
+		List<Users> user1 = userInterface.findAll();
 		return user1;
 	}
 
 	@Override
-	public User3 findByUsername(String name) {
-		 User3 user1 = userInterface.findByUsername(name);
+	public Users findByUsername(String name) {
+		 Users user1 = userInterface.findByUsername(name);
 		return user1;
 	}
 
