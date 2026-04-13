268a645565813492e4f6e82e9787f131ed1892bb
Indraja-Kallukunta <kindraja399@gmail.com>
2026-04-13 08:49:48 +0530

    EntriesInterface.java

--- src/main/java/com/twg/Springboot/Repository/EntriesInterface.java
+++ src/main/java/com/twg/Springboot/Repository/EntriesInterface.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.twg.Springboot.Entities.EntriesOfUser;
-import com.twg.Springboot.Entities.User3;
+import com.twg.Springboot.Entities.Users;
 
 public interface EntriesInterface extends JpaRepository<EntriesOfUser, Long> {
 	List<EntriesOfUser> findByUserid(long id);
